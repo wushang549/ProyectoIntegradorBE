fb5a8d78368d67a69a05c41d24828fae8162b153
wushang549 <diegohernandez549@yahoo.com.mx>
2026-02-16 22:05:32 -0800

    readme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from db import init_db
-
 app = FastAPI()
 
 app.add_middleware(
@@ -18,12 +16,5 @@ app.add_middleware(
 
 # routers
 from api.granulate import router as granulate_router
-from api.projects import router as projects_router
 
 app.include_router(granulate_router)
-app.include_router(projects_router)
-
-
-@app.on_event("startup")
-def on_startup() -> None:
-    init_db()
